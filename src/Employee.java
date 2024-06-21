25be3be8cd44bda273978420065b21474b07a93d
TatianaGold <ta-nit@yandex.ru>
2024-06-21 19:20:10 +0300

    lesson_6

---
+++ src/Employee.java
@@ -0,0 +1,27 @@
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+
+    public Employee(String fullName, String position, String email, String phone, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("\nСотрудник: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+}
